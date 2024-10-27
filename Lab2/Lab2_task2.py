c7919d7d6091f7f9014d5342348c418cee391cac
Granikita <granikita2006@gmail.com>
2024-10-27 18:47:50 +0300

    Update Lab2_task2.py
    Исправил ошибку, кажется оптимальный вариант

--- Lab2/Lab2_task2.py
+++ Lab2/Lab2_task2.py
@@ -7,4 +7,4 @@ increase = 0.03  # Ежемесячный рост цен
 money_capital = 0
 for i in range(months):
     money_capital += spend*(((increase+1)**i)) - salary
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", int(money_capital))
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital+0.5))
