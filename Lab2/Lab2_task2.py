3e4418907edd084e130cc46de030ebb0a8bc168c
Granikita <granikita2006@gmail.com>
2024-10-28 18:39:12 +0300

    Update Lab2_task2.py
    Исправил, переделал на math.ceil

--- Lab2/Lab2_task2.py
+++ Lab2/Lab2_task2.py
@@ -1,3 +1,5 @@
+import math
+
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
@@ -7,4 +9,4 @@ increase = 0.03  # Ежемесячный рост цен
 money_capital = 0
 for i in range(months):
     money_capital += spend*(((increase+1)**i)) - salary
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital+0.5))
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", math.ceil(money_capital))
